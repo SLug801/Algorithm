9e4e10defd50ea760f8f824f335285dd8efe4a9a
SLug801 <jw7581171@gmail.com>
2026-04-13 00:15:13 +0900

    Update 2440.java

--- 2440.java
+++ 2440.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main{
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        int n = scan.nextInt;
+        int n = scan.nextInt();
         
         for(int i=0; i<n; i++){    
             for(int j=0; j<n-i; j++){
